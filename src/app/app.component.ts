93b1aa2fef34026e19fd622beb4f994f6a695692
Zack Kayser <zack.kayser@plxis.com>
2017-09-28 21:22:18 -0400

    Bring in HTTP client and experiment with get calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title: string = 'app';
+  greeting: string;
+  from: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:4000/api/hello').subscribe(data => {
+      console.log(`Received data from API: ${JSON.stringify(data)}`);
+      this.greeting = data["greeting"];
+      this.from = data["from"];
+    });
+  }
 }
